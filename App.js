3657f70cfd2698aadd42abd9010160f2fb74d3b7
Caleb Han <102847271+kidscoots101@users.noreply.github.com>
2023-06-23 18:10:13 +0800

    ui done

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Time from "./components/Time";
+import Duration from "./components/Duration";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text
+        style={{
+          fontWeight: "bold",
+          fontSize: 35,
+          padding: 30,
+          marginTop: 100,
+        }}
+      >
+        Time
+      </Text>
+      <Time />
+      <Text
+        style={{
+          fontWeight: "bold",
+          fontSize: 35,
+          padding: 30,
+        }}
+      >
+        Duration
+      </Text>
+      <Duration />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
